26137ea05353bba98ea37d5868b0f50335214a86
aturner-hash <aturner@mtlawoffice.com>
2026-06-16 12:04:38 -0400

    Change sfProxyUrl in config.js
    Updated sfProxyUrl in config.js with a new code.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js — DO NOT commit this file to Git
 window.MT_CONFIG = {
-  sfProxyUrl: 'https://mt-sharefile-proxy-eudbfhfydhaad3fr.eastus-01.azurewebsites.net/api/sharefile-proxy?code=SG-dB-HI7jhcJM2xGum9_MvJrBcY84-JcDYdXib7wXwWAzFux4SxWw=='
+  sfProxyUrl: 'https://mt-sharefile-proxy-eudbfhfydhaad3fr.eastus-01.azurewebsites.net/api/sharefile-proxy?code=QaeqJO8PqWHngbDu_aqbw40XG4igRE6THAozdIgkoadsAzFu1DO0Ug=='
 };
