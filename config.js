1286549b4fea726367676692172f609ac68d0d05
aturner-hash <aturner@mtlawoffice.com>
2026-06-16 12:14:22 -0400

    Remove sensitive code from config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js — DO NOT commit this file to Git
 window.MT_CONFIG = {
-  sfProxyUrl: 'https://mt-sharefile-proxy-eudbfhfydhaad3fr.eastus-01.azurewebsites.net/api/sharefile-proxy?code=wbm05NflhJtnEP60cxcfFxKg3j92K5nx6tWZxhG2vn8tAzFugPeBTA=='
+  sfProxyUrl: 'https://mt-sharefile-proxy-eudbfhfydhaad3fr.eastus-01.azurewebsites.net/api/sharefile-proxy'
 };
