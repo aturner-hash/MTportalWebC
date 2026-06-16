b9afe5cffd0245d539c41ffa8684e85509b4e15d
aturner-hash <aturner@mtlawoffice.com>
2026-06-16 11:20:44 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js — DO NOT commit this file to Git
+window.MT_CONFIG = {
+  sfProxyUrl: 'https://mt-sharefile-proxy-eudbfhfydhaad3fr.eastus-01.azurewebsites.net/api/sharefile-proxy?code=SG-dB-HI7jhcJM2xGum9_MvJrBcY84-JcDYdXib7wXwWAzFux4SxWw=='
+};
